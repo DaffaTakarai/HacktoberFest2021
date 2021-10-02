4fc410ec8d3db8a9accfa659f8b955df72e29083
Mayank <mayankbhatt07141@gmail.com>
2021-10-03 03:59:03 +0530

    feat:added a java program to find the set bits in any number using brian kernighan algorithm

--- CountsetBits.java
+++ CountsetBits.java
@@ -1,11 +1,11 @@
-/*brian kernighan algorithm - 
+/*brian Kernighan algorithm - 
 if we do n&n-1 then all bits after rightmost set bit are flipped including rightmost bit and each time 
 we increment count value*/ 
 import java.util.Scanner;
 public class CountsetBits {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Enter a number: ");
         int n=s.nextInt();
         s.close();
         int count=0;
@@ -13,6 +13,6 @@ public class CountsetBits {
             count++;
             n=n&(n-1);
         }
-         System.out.println("number of set bits are " +count);
+         System.out.println(" Total number of set bits are " +count);
     }
 }
