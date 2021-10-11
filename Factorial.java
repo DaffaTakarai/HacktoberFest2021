a6bccc9158959896940a9f2796c98379dd8d4785
Ujjwal Dhakal <68050149+ujjwalnp@users.noreply.github.com>
2021-10-11 09:37:13 +0545

    Create Factorial.java
    I created the Java program to find factorial of a given number.

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int n = sc.nextInt();
+		int result = 1;
+		for (int i=n; i>=1; i--) {
+			result = result * i;
+		}System.out.println("Your factorial is "+result);
+
+	}
+
+}
